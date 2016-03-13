5509852cca3ec69ba86cf0bc8d3a4d0e62856857
Sergio Jadir Ribeiro Leite <sergio_jadir@outlook.com>
2016-03-12 21:42:43 -0300

    Adiconar rails ao gemfile.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
 
+gem "rails", "~> 4.2.2"
+
 # Specify your gem's dependencies in outdated_browser.gemspec
 gemspec
--- lib/outdated_browser.rb
+++ lib/outdated_browser.rb
@@ -2,6 +2,5 @@ require "outdated_browser/version"
 require "browser"
 
 module OutdatedBrowser
+	require "outdated_browser/rails"
 end
-
-require "outdated_browser/rails"
